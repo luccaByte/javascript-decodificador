8fdd762a9ba44a446dfbba010fba0f8ab9fca6a0
Lucas Gonçalves <103429796+luccaByte@users.noreply.github.com>
2024-08-06 20:10:29 +0000

    feat: capturando elementos do html

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const textarea = document.getElementById('text-input');
+const textareaValue = document.getElementById('text-input').value;
+const btnEncrypt = document.getElementById('encrypt');
+const btnDecrypt = document.getElementById('decrypt');
+const outputText = document.querySelector('.output-text');
+const outputAlert = document.querySelector('.output-alert');
+
+    
