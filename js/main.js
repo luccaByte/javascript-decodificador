4fba3746bfc53766ec18f3c69022a16c4717d673
Lucas Gonçalves <103429796+luccaByte@users.noreply.github.com>
2024-08-07 20:27:14 +0000

    feat: função para a criptografia

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
 const textarea = document.getElementById('text-input');
-const textareaValue = document.getElementById('text-input').value;
 const btnEncrypt = document.getElementById('encrypt');
 const btnDecrypt = document.getElementById('decrypt');
 const outputText = document.querySelector('.output-text');
 const outputAlert = document.querySelector('.output-alert');
 
-    
+function encrypt() {
+    const textareaValue = textarea.value;
+
+    let result = textareaValue
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
+
+    outputText.innerHTML = `<p id="result">${result}</p>`;
+};
+
+function decrypt() {
+    const textareaValue = textarea.value;
+
+    let result = textareaValue
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
+
+    outputText.innerHTML = `<p id="result">${result}</p>`;
+}
+
+btnEncrypt.addEventListener("click", () => {
+    outputAlert.style.display = "none";
+    encrypt();
+});
+
+btnDecrypt.addEventListener("click", () => {
+    outputAlert.style.display = "none";
+    decrypt();
+});
