03e66fc175d2228cbb1b457c41b94a74f600a58d
Lucas Gonçalves <103429796+luccaByte@users.noreply.github.com>
2024-08-08 19:26:08 +0000

    feat: botao para copiar texto

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-const textarea = document.getElementById('text-input');
+const textArea = document.getElementById('text-input');
 const btnEncrypt = document.getElementById('encrypt');
 const btnDecrypt = document.getElementById('decrypt');
 const outputText = document.querySelector('.output-text');
 const outputAlert = document.querySelector('.output-alert');
+const btnOutputCopy = document.getElementById('output-copy');
 
 function encrypt() {
-    const textareaValue = textarea.value;
+    const textareaValue = textArea.value;
 
     let result = textareaValue
     .replace(/e/g, 'enter')
@@ -18,7 +19,7 @@ function encrypt() {
 };
 
 function decrypt() {
-    const textareaValue = textarea.value;
+    const textareaValue = textArea.value;
 
     let result = textareaValue
     .replace(/enter/g, 'e')
@@ -28,14 +29,32 @@ function decrypt() {
     .replace(/ufat/g, 'u');
 
     outputText.innerHTML = `<p id="result">${result}</p>`;
-}
+};
 
 btnEncrypt.addEventListener("click", () => {
     outputAlert.style.display = "none";
+    btnOutputCopy.style.display = "block";
+    outputText.classList.add('active');
     encrypt();
 });
 
 btnDecrypt.addEventListener("click", () => {
     outputAlert.style.display = "none";
+    btnOutputCopy.style.display = "block";
+    outputText.classList.add('active');
     decrypt();
 });
+
+function copy() {
+    const resultText = document.getElementById('result').textContent;
+
+    navigator.clipboard.writeText(resultText)
+    .then(() => {
+        alert('Texto copiado para a área de transferência!');
+    })
+    .catch(err => {
+        console.error('Erro ao copiar texto: ', err);
+    });
+}
+
+btnOutputCopy.addEventListener('click', copy);
