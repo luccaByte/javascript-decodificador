1e8bfd768557b957dd4dc60acb6801f820f8e7a3
Lucas Gonçalves <103429796+luccaByte@users.noreply.github.com>
2024-08-20 16:50:40 +0000

    fix: o mes foi arrumado

--- js/date.js
+++ js/date.js
@@ -3,12 +3,11 @@ function time() {
     h = today.getHours();
     m = today.getMinutes();
     day = today.getDate();
-    month = today.getMonth();
+    month = today.getMonth()+1;
     year = today.getFullYear();
 
     document.getElementById('time').innerHTML = `${h}:${m}`;
     setTimeout('time()',500);
 
     document.getElementById('date').innerHTML = `${day}/${month}/${year}`;
-    
 }
